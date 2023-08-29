1b4ca3b1caf5663894cd9fc3f59dbe4ba321e3f3
alimangat <ali.raza.mangat5@gmail.com>
2023-08-29 14:27:02 +0500

    Signup pages created, validations and flashes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root 'main#home'
   
-  get '/about', to: 'main#about'
+  get "about", to: "main#about"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
 end
