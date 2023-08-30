8a6f398787c0026325883d3b13f7e926c950d3fa
alimangat <ali.raza.mangat5@gmail.com>
2023-08-30 22:01:42 +0500

    SignIn and Signup pages added to navbar and testing done

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,5 @@
 class MainController < ApplicationController
-    def home
-      if session[:user_id]
-        @user = User.find_by(id: session[:user_id])
-    end
+  def home
   end
 
   def about
